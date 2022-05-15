0439e3366e5717b3b39b78d26722ad65f594b499
BrianZ <brianz.zyn@gmail.com>
2022-05-15 00:41:32 -0700

    local: integrate uhd

---
+++ uhd/host/Brian/configuration_rx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# use "chmod +x ./filename.sh" to make file executable
+# serial operate the commands
+
+cd ~
+ifconfig data1 10.38.1.4 netmask 255.255.0.0 mtu 9000 up &&
+sleep 1
+sysctl -w net.core.wmem_max=62500000 &&
+sleep 0.5
+sysctl -w net.core.rmem_max=62500000
