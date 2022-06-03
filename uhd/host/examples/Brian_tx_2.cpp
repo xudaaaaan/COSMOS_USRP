3dab75487e81a707df3f21b231fef7ab24815743
BrianZ <brianz.zyn@gmail.com>
2022-06-02 23:34:42 -0700

    local: test to print starting tick 1

--- uhd/host/examples/Brian_tx_2.cpp
+++ uhd/host/examples/Brian_tx_2.cpp
@@ -374,13 +374,13 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
         // uhd::time_spec_t Ettus_const_value;
         // Ettus_const_value.time_spec_t(0.1);
         
-        std::cout << "Ettus timestamp value = " << uhd::time_spec_t(0.1).get_real_secs() << std::endl;
-        // md.time_spec      = usrp->get_time_now() + uhd::time_spec_t(0.1);
+        // std::cout << "Ettus timestamp value = " << uhd::time_spec_t(0.1).get_real_secs() << std::endl;
+        md.time_spec      = usrp->get_time_now() + uhd::time_spec_t(0.1);
         // md.time_spec = usrp->get_time_now() + Ettus_const_value;
 
         
         
-        // std::cout << "starting timetamp = " << md.time_spec << std::endl;
+        std::cout << "starting timetamp = " << md.time_spec.get_real_secs() << std::endl;
 
 
 
