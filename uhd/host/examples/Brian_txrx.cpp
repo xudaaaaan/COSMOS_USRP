f0afccf49f0f60bd7ad130a76d08821c0072c2e5
BrianZ <brianz.zyn@gmail.com>
2022-06-08 00:22:59 -0700

    local: modified the Brian_tx_2 and Brian_rx, create Brian_txrx.cpp

--- uhd/host/examples/Brian_txrx.cpp
+++ uhd/host/examples/Brian_txrx.cpp
@@ -721,8 +721,8 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
         tx_sensor_names = tx_usrp->get_tx_sensor_names(tx_sensor_chan);
 
                     std::cout << std::endl;
-                    std::cout << "Test: Tx sensor name: " << tx_sensor_names[0]
-                            << std::endl;
+                    // std::cout << "Test: Tx sensor name: " << tx_sensor_names[0]
+                    //         << std::endl;
 
         if (std::find(tx_sensor_names.begin(), tx_sensor_names.end(), "lo_locked")
             != tx_sensor_names.end()) {
@@ -736,8 +736,8 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
         rx_sensor_names = rx_usrp->get_rx_sensor_names(rx_sensor_chan);
 
                     std::cout << std::endl;
-                    std::cout << "Test: Rx sensor name: " << rx_sensor_names[0]
-                            << std::endl;
+                    // std::cout << "Test: Rx sensor name: " << rx_sensor_names[0]
+                    //         << std::endl;
 
         if (std::find(rx_sensor_names.begin(), rx_sensor_names.end(), "lo_locked")
             != rx_sensor_names.end()) {
@@ -756,8 +756,8 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
             uhd::sensor_value_t ref_locked = tx_usrp->get_mboard_sensor("ref_locked", 0);
 
                         std::cout << std::endl;
-                        std::cout << "Test: Tx mboard sensor name: " << tx_sensor_names[0]
-                                << std::endl;
+                        // std::cout << "Test: Tx mboard sensor name: " << tx_sensor_names[0]
+                        //         << std::endl;
 
             std::cout << boost::format("Checking Tx: %s ...") % ref_locked.to_pp_string()
                     << std::endl;
@@ -771,8 +771,8 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
             uhd::sensor_value_t ref_locked = rx_usrp->get_mboard_sensor("ref_locked", 0);
 
                         std::cout << std::endl;
-                        std::cout << "Test: Rx mboard sensor name: " << rx_sensor_names[0]
-                                << std::endl;
+                        // std::cout << "Test: Rx mboard sensor name: " << rx_sensor_names[0]
+                        //         << std::endl;
 
             std::cout << boost::format("Checking Rx: %s ...") % ref_locked.to_pp_string()
                     << std::endl;
