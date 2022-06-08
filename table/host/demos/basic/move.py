2e48e203c8578213a2a1133768c914c12a6c9e74
BrianZ <brianz.zyn@gmail.com>
2022-06-08 09:45:19 -0700

    local: improve the control logic of move.py

--- table/host/demos/basic/move.py
+++ table/host/demos/basic/move.py
@@ -31,58 +31,97 @@ def main():
     # Create an argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--node", type=str, default='srv1-in1', help="COSMOS-SB1 node name (i.e., srv1-in1)")
+    parser.add_argument("-g", "--group", help="if use para group, then control 2 XY tables together")
     parser.add_argument("--comb", type=int, default=1, help="combination of the XY table positions")
     parser.add_argument("--x", type=int, default=0, help="x coordinate on XY table")
     parser.add_argument("--y", type=int, default=0, help="y coordinate on XY table")
-    parser.add_argument("--angle", type=int, default=0, help="angle of the array on XY table")
+    parser.add_argument("--a", type=int, default=0, help="angle of the array on XY table")
     args = parser.parse_args()
 
     # Create a configuration parser
     config = configparser.ConfigParser()
     config.read('../../config/sivers.ini')
 
-    xytable0 = mmwsdr.utils.XYTable(config[args.node]['table_name'], isdebug=isdebug)
+    
 
-    if not args.comb:
-        x = args.x
-        y = args.y
-        angle = args.angle
+    if args.group:
+        xytable1 = mmwsdr.utils.XYTable(config["srv1-in1"]['table_name'], isdebug=isdebug)
+        xytable2 = mmwsdr.utils.XYTable(config["srv1-in2"]['table_name'], isdebug=isdebug)
+        if args.comb == 1:
+            x1 = 0
+            y1 = 0
+            a1 = 0
+            x2 = 1300
+            y2 = 0
+            a2 = 0
+        elif args.comb == 2:
+            x1 = 0
+            y1 = 1300
+            a1 = 0
+            x2 = 1300
+            y2 = 0
+            a2 = 0
+        elif args.comb == 3:
+            x1 = 0
+            y1 = 0
+            a1 = 0
+            x2 = 1300
+            y2 = 1300
+            a2 = 0
+        elif args.comb == 4:
+            x1 = 0
+            y1 = 1300
+            a1 = 0
+            x2 = 1300
+            y2 = 1300
+            a2 = 0
+        xytable1.move(x1, y1, a1)
+        xytable2.move(x2, y2, a2)
     else:
-        if args.comb == 1 and args.node == "srv1-in1":  # For table 1:
-            x = 0
-            y = 0
-            angle = 0
-        elif args.comb == 2 and args.node == "srv1-in1":
-            x = 0
-            y = 1300
-            angle = 0
-        elif args.comb == 3 and args.node == "srv1-in1":
-            x = 0
-            y = 0
-            angle = 0
-        elif args.comb == 4 and args.node == "srv1-in1":
-            x = 0
-            y = 1300
-            angle = 0
-        elif args.comb == 1 and args.node == "srv1-in2":    # For table 2:
-            x = 1300
-            y = 0
-            angle = 0
-        elif args.comb == 2 and args.node == "srv1-in2":
-            x = 1300
-            y = 0
-            angle = 0
-        elif args.comb == 3 and args.node == "srv1-in2":
-            x = 1300
-            y = 1300
-            angle = 0
-        elif args.comb == 4 and args.node == "srv1-in2":
-            x = 1300
-            y = 1300
-            angle = 0            
+        xytable0 = mmwsdr.utils.XYTable(config[args.node]['table_name'], isdebug=isdebug)
+        if not args.comb:
+            x = args.x
+            y = args.y
+            a = args.a
+        else:
+            if args.node == "srv1-in1":
+                if args.comb == 1:
+                    x = 0
+                    y = 0
+                    a = 0
+                elif args.comb == 2:
+                    x = 0
+                    y = 1300
+                    a = 0
+                elif args.comb == 3:
+                    x = 0
+                    y = 0
+                    a = 0
+                elif args.comb == 4:
+                    x = 0
+                    y = 1300
+                    a = 0
+            elif args.node == "srv1-in2":
+                if args.comb == 1:
+                    x = 1300
+                    y = 0
+                    a = 0
+                elif args.comb == 2:
+                    x = 1300
+                    y = 0
+                    a = 0
+                elif args.comb == 3:
+                    x = 1300
+                    y = 1300
+                    a = 0
+                elif args.comb == 4:
+                    x = 1300
+                    y = 1300
+                    a = 0         
+        xytable0.move(x, y, a)
 
 
-    xytable0.move(x, y, angle)
+    
 
 
     # t = threading.Thread(target=xytable0.video(t=args.videotime))
