763b721951c3bb325129adab31556bbbba7d5b98
BrianZ <brianz.zyn@gmail.com>
2022-06-07 23:40:26 -0700

    local: modified the Brian_tx_2 and Brian_rx, create Brian_txrx.cpp

--- uhd/host/examples/wavetable_Brian.hpp
+++ uhd/host/examples/wavetable_Brian.hpp
@@ -22,7 +22,7 @@ class wave_table_class_Brian
 {
 public:
     wave_table_class_Brian(const std::string& wave_type, const float ampl, std::string signal_file)
-        : _wave_table(wave_table_len)
+        : _wave_table_Brian(wave_table_len)
     {
         // compute real wave table with 1.0 amplitude
         std::vector<float> real_wave_table(wave_table_len);
@@ -63,7 +63,7 @@ public:
                     reader >> imag_wave_table_tmp;
                     imag_wave_table[i] = imag_wave_table_tmp;
 
-                    _wave_table[i] = 
+                    _wave_table_Brian[i] = 
                         std::complex<float>(ampl * real_wave_table[i], ampl * imag_wave_table[i]);
                 }
             }
@@ -80,7 +80,7 @@ public:
             }
 
             for (size_t i = 0; i < wave_table_len; i++) {
-                _wave_table[i] = std::complex<float>(ampl * real_wave_table[i], 
+                _wave_table_Brian[i] = std::complex<float>(ampl * real_wave_table[i], 
                 ampl * imag_wave_table[i]);
             }
         } else
@@ -104,9 +104,9 @@ public:
 
     inline std::complex<float> operator()(const size_t index) const
     {
-        return _wave_table[index % wave_table_len];
+        return _wave_table_Brian[index % wave_table_len];
     }
 
 private:
-    std::vector<std::complex<float>> _wave_table;
+    std::vector<std::complex<float>> _wave_table_Brian;
 };
