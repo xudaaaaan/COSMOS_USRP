7120b31ad621a546772bde2eb9ccfe368c5cd051
BrianZ <brianz.zyn@gmail.com>
2023-05-25 21:32:24 -0400

    updated video.py to make it independent from srv1-in1/2

--- table/host/demos/basic/video.py
+++ table/host/demos/basic/video.py
@@ -30,29 +30,31 @@ def main():
 
     # Create an argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--node", type=str, default='srv1-in1', help="COSMOS-SB1 node name (i.e., srv1-in1)")
-    parser.add_argument("--x", type=int, default=0, help="x coordinate on XY table")
-    parser.add_argument("--y", type=int, default=0, help="y coordinate on XY table")
-    parser.add_argument("--a", type=int, default=0, help="angle of the array on XY table")
-    parser.add_argument("--t", dest='videotime', type=int, default=10800, help="video duration")
+    # parser.add_argument("-n", "--node", type=str, default='srv1-in1', help="COSMOS-SB1 node name (i.e., srv1-in1)")
+    parser.add_argument("-i", "--index", type=str, default='2', help="the index of the XY table you want to monitor")
+    parser.add_argument("-X", "--x", type=int, default=0, help="x coordinate on XY table")
+    parser.add_argument("-Y", "--y", type=int, default=0, help="y coordinate on XY table")
+    parser.add_argument("-R", "--r", type=int, default=0, help="rotation angle of the array on XY table")
+    parser.add_argument("-T", "--Time", dest='videotime', type=int, default=10800, help="video duration")
     args = parser.parse_args()
 
-    # Create a configuration parser
-    config = configparser.ConfigParser()
-    config.read('../../config/sivers.ini')
+    # # Create a configuration parser
+    # config = configparser.ConfigParser()
+    # config.read('../../config/sivers.ini')
 
-    xytable0 = mmwsdr.utils.XYTable(config[args.node]['table_name'], isdebug=isdebug)
+    # xytable_obj = mmwsdr.utils.XYTable(config[args.node]['table_name'], isdebug=isdebug)
+    xytable_obj = mmwsdr.utils.XYTable('xytable'+args.index, isdebug=isdebug)
 
     if args.x and args.y and args.a:
-        xytable0.move(x=args.x, y=args.y, angle=args.a)
+        xytable_obj.move(x=args.x, y=args.y, angle=args.a)
 
 
-    t = threading.Thread(target=xytable0.video(t=args.videotime))
+    t = threading.Thread(target=xytable_obj.video(t=args.videotime))
     t.daemon = True
     t.start()
 
     # Create a move
-    # xytable0.move(x=500, y=500, angle=-45)
+    # xytable_obj.move(x=500, y=500, angle=-45)
 
     t.join()
 
